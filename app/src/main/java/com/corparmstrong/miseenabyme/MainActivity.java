dfa3917eeda2069f63bdc477057d88a3ae193939
Dmitriy Fedykovich <punkvirus@mail.ru>
2015-09-12 16:59:34 +0300

    Initial commit

---
+++ app/src/main/java/com/corparmstrong/miseenabyme/MainActivity.java
@@ -0,0 +1,57 @@
+package com.corparmstrong.miseenabyme;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String WEBSITE_URL = "https://www.apostlemod.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void OnLoginClick(View view) {
+        startActivity(new Intent(MainActivity.this, RemoteAccessActivity.class));
+    }
+
+    public void OnCreateNewAccountClick(View view) {
+        Toast.makeText(MainActivity.this, "Feature not available.", Toast.LENGTH_LONG).show();
+    }
+
+    public void onWebsiteClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(WEBSITE_URL));
+        startActivity(intent);
+    }
+}
